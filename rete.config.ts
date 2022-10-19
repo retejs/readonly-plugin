bd356476b28d1289591ad10a5dd70d51bfdbd3f0
Ni55aN <huccah@gmail.com>
2022-10-20 00:15:09 +0300

    wip

--- rete.config.ts
+++ rete.config.ts
@@ -1,9 +1,5 @@
 export default {
     input: 'src/index.ts',
     name: 'ReadonlyPlugin',
-    babelPresets: [
-        require('@babel/preset-typescript')
-    ],
-    extensions: ['.js', '.ts'],
     globals: { 'rete': 'Rete' }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,39 @@
-/// <reference types="rete-connection-plugin" />
-import { NodeEditor } from 'rete';
+// /// <reference types="rete-connection-plugin" />
+// import { NodeEditor } from 'rete';
 
-declare module 'rete/types/events' {
-    interface EventsTypes {
-        isreadonly: void;
-        readonly: boolean;
-    }
-}
+// declare module 'rete/types/events' {
+//     interface EventsTypes {
+//         isreadonly: void;
+//         readonly: boolean;
+//     }
+// }
 
-function install(editor: NodeEditor, params: { enabled?: boolean }) {
-    editor.bind('isreadonly');
-    editor.bind('readonly');
+// function install(editor: NodeEditor, params: { enabled?: boolean }) {
+//     editor.bind('isreadonly');
+//     editor.bind('readonly');
 
-    if (params.enabled !== false) params.enabled = true;
+//     if (params.enabled !== false) params.enabled = true;
 
-    editor.on('isreadonly', () => params.enabled);
-    editor.on('readonly', enabled => {
-        params.enabled = enabled;
-    });
+//     editor.on('isreadonly', () => params.enabled);
+//     editor.on('readonly', enabled => {
+//         params.enabled = enabled;
+//     });
 
-    editor.on([
-        'keydown',
-        'nodetranslate',
-        'nodeselect',
-        'connectioncreate',
-        'connectionremove',
-        'nodecreate',
-        'noderemove',
-        'connectionpick'
-    ], () => editor.silent || params.enabled !== true);
-}
+//     editor.on([
+//         'keydown',
+//         'nodetranslate',
+//         'nodeselect',
+//         'connectioncreate',
+//         'connectionremove',
+//         'nodecreate',
+//         'noderemove',
+//         'connectionpick'
+//     ], () => editor.silent || params.enabled !== true);
+// }
 
-export default {
-    name: 'readonly',
-    install
-}
+// export default {
+//     name: 'readonly',
+//     install
+// }
+
+export * from './next'
