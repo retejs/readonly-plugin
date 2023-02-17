ff5f182cc4fde831c59f5c8dcb39f789baa426da
Ni55aN <huccah@gmail.com>
2023-02-17 22:59:49 +0200

    enable/disable dinamically

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,37 @@ import { BaseSchemes, Root, Scope } from 'rete'
 import { Area2D } from 'rete-area-plugin'
 import { Connection } from 'rete-connection-plugin'
 
-console.log('readonly')
-
 export class ReadonlyPlugin<Schemes extends BaseSchemes> {
-    root = new Scope<never, [Root<Schemes>]>('readonly')
-    area = new Scope<never, [Area2D<Schemes>, Root<Schemes>]>('readonly')
-    connection = new Scope<never, [Connection, Root<Schemes>]>('readonly')
+  root = new Scope<never, [Root<Schemes>]>('readonly')
+  area = new Scope<never, [Area2D<Schemes>, Root<Schemes>]>('readonly')
+  connection = new Scope<never, [Connection, Root<Schemes>]>('readonly')
 
-    constructor() {
-        this.root.addPipe(context => {
-            if (context.type === 'nodecreate') return
-            if (context.type === 'connectioncreate') return
-            return context
-        })
-        this.area.addPipe(context => {
-            if (context.type === 'nodetranslate') return
-            return context
-        })
-        this.connection.addPipe(context => {
-            if (context.type === 'connectionpick') return
-            return context
-        })
-    }
+  enabled = false
+
+  constructor() {
+    this.root.addPipe(context => {
+      if (!this.enabled) return context
+      if (context.type === 'nodecreate') return
+      if (context.type === 'connectioncreate') return
+      return context
+    })
+    this.area.addPipe(context => {
+      if (!this.enabled) return context
+      if (context.type === 'nodetranslate') return
+      return context
+    })
+    this.connection.addPipe(context => {
+      if (!this.enabled) return context
+      if (context.type === 'connectionpick') return
+      return context
+    })
+  }
+
+  public enable() {
+    this.enabled = true
+  }
+
+  public disable() {
+    this.enabled = false
+  }
 }
